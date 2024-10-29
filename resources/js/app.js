2ccaecd673a5ac70b68b7939f73cda1fb7971906
GuilhermeTiede <guilhermediastiede@gmail.com>
2024-10-29 16:09:52 -0300

    [add] Adiciona extensões e configura as mesmas.

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,17 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-
+import { ZiggyVue } from 'ziggy-js';
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import { createPinia } from 'pinia';
+import { mask } from 'vue-the-mask';
+import { VMoney } from 'v-money';
+import Vue3Toastify from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const vuetify = createVuetify();
+
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -16,9 +24,16 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
+        const pinia = createPinia();
+
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
+            .use(ZiggyVue, Ziggy)
+            .use(vuetify)
+            .use(Vue3Toastify)
+            .use(pinia)
+            .directive('mask', mask)
+            .directive('money', VMoney)
             .mount(el);
     },
     progress: {
